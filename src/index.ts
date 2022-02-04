4d03a6bb6b4698f561abdba29571cf76c362d900
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-03 23:06:36 -0500

    Set up basic pieces, without dependency injection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
-const world = 'world';
+import got from 'got';
 
-export function hello(world: string = world): string {
-  return `Hello ${world}! `;
+interface IApiManager<T> {
+  fetchData(): Promise<T>;
+}
+
+type Todo = {
+  id: number;
+  completed: boolean;
+  title: string;
+  userId: number;
+};
+
+class ApiManager implements IApiManager<Todo[]> {
+  _todoClient: TodoClient;
+
+  constructor(todoClient: TodoClient) {
+    this._todoClient = todoClient;
+  }
+
+  async fetchData(): Promise<Todo[]> {
+    return this._todoClient.getTodos();
+  }
+}
+
+class TodoClient {
+  _resourceUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  async getTodos(): Promise<Todo[]> {
+    const todos: Todo[] = await got(this._resourceUrl).json();
+    return todos;
+  }
 }
