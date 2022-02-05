55c074be7b61092aa58f4fd10fb892b6f85e8ec4
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-02-04 20:15:34 -0500

    Set up a little example to show my code works

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import container from './inversify.config';
 import { ApiManager } from './classes';
+
+(async function () {
+  console.log('Fetching todo data...');
+
+  const apiManager = container.get<ApiManager>(ApiManager);
+  const todos = await apiManager.fetchData();
+
+  console.log('Here is the first todo from the received data:', todos[0]);
+})();
